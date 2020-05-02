9a6e3c426e7cbefe38460f7b981a533e254588ca
Tylon Guan <Tylon.guan@stonybrook.edu>
2020-05-02 13:44:30 -0400

    Added a new photo to home slide to index.html

--- assets/gallery/script.js
+++ assets/gallery/script.js
@@ -68,37 +68,39 @@
         };
     }
 
-    function updateMasonry(event){
+    function updateMasonry(event) {
         var $section = $(event.target);
         if (typeof $.fn.masonry !== 'undefined') {
             $section.outerFind('.mbr-gallery').each(function() {
-                var $msnr = $(this).find('.mbr-gallery-row').masonry({
-                    itemSelector: '.mbr-gallery-item:not(.mbr-gallery-item__hided)',
-                    percentPosition: true,
-                    horizontalOrder: true
-                });
-                // reload masonry (need for adding new or re-sort items)
-                $msnr.masonry('reloadItems');
-                $msnr.on('filter', function() {
-                    $msnr.masonry('reloadItems');
-                    $msnr.masonry('layout');
-                    // update parallax backgrounds
-                    $(window).trigger('update.parallax');
-                }.bind(this, $msnr));
-                // layout Masonry after each image loads
+                var $msnr = $(this).find('.mbr-gallery-row');
                 $msnr.imagesLoaded().progress(function() {
-                    $msnr.masonry('layout');
-                });
+                    $msnr.masonry({
+                        itemSelector: '.mbr-gallery-item:not(.mbr-gallery-item__hided)',
+                        percentPosition: true,
+                        horizontalOrder: true
+                    });
+
+                    // reload masonry (need for adding new or re-sort items)
+                    $msnr.masonry('reloadItems');
+                
+                    $msnr.on('filter', function() {
+                        $msnr.masonry('reloadItems');
+                        $msnr.masonry('layout');
+                        // update parallax backgrounds
+                        $(window).trigger('update.parallax');
+                    }.bind(this, $msnr));
+                    $msnr.imagesLoaded().progress(function() {
+                        $msnr.masonry('layout');
+                    });
+                })
             });
         }
     };
 
     /* Masonry Grid */
     $(document).on('add.cards', function(event) {
-        var $section = $(event.target),
-            allItem = $section.find('.mbr-gallery-filter-all');
-        var filterList = [];
-        $section.on('click', '.mbr-gallery-filter li > .btn', function(e) {
+        var $section = $(event.target);
+        $section.on('click', '.mbr-gallery-filter li', function(e) {
             e.preventDefault();
             var $li = $(this).closest('li');
 
@@ -106,7 +108,7 @@
             $li.addClass('active');
 
             var $mas = $li.closest('section').find('.mbr-gallery-row');
-            var filter = $(this).html().trim();
+            var filter = $(this)[0].textContent.trim();
 
             $section.find('.mbr-gallery-item').each(function(i, el) {
                 var $elem = $(this);
@@ -128,36 +130,49 @@
 
             $mas.closest('.mbr-gallery-row').trigger('filter');
         });
-    })
+    });
+
+    if (isBuilder) {
+        $(document).on('changeButtonColor.cards', function(event) {
+            var $section = $(event.target);
+
+            if ($section.find('.mbr-gallery-filter').length > 0 && $(event.target).find('.mbr-gallery-filter').hasClass('gallery-filter-active')) {
+                var classAttr = ($section.find('.mbr-gallery-filter .mbr-gallery-filter-all').find('a').attr('class') || '').replace(/(^|\s)active(\s|$)/, ' ').trim();
+
+                $section.find('.mbr-gallery-filter ul li:not(.mbr-gallery-filter-all) a').attr('class', classAttr);
+            }
+
+            updateMasonry(event);
+        });
+    }
+
     $(document).on('add.cards changeParameter.cards', function(event) {
-        var $section = $(event.target),
-            allItem = $section.find('.mbr-gallery-filter-all');
+        var $section = $(event.target);
         var filterList = [];
+
         $section.find('.mbr-gallery-item').each(function(el) {
-            var tagsAttr = ($(this).attr('data-tags') || "").trim();
-            var tagsList = tagsAttr.split(',');
+            var tagsList = ($(this).attr('data-tags') || "").trim().split(',');
 
             tagsList.map(function(el) {
-                var tag = el.trim();
-
-                if ($.inArray(tag, filterList) === -1)
-                    filterList.push(tag);
+                el = el.trim();
+                if ($.inArray(el, filterList) === -1) filterList.push(el);
             });
         });
 
         if ($section.find('.mbr-gallery-filter').length > 0 && $(event.target).find('.mbr-gallery-filter').hasClass('gallery-filter-active')) {
             var filterHtml = '';
 
-            $section.find('.mbr-gallery-filter ul li:not(li:eq(0))').remove();
+            $section.find('.mbr-gallery-filter ul li:not(.mbr-gallery-filter-all)').remove();
 
+            var allItem = $section.find('.mbr-gallery-filter .mbr-gallery-filter-all'),
+                classAttr = (allItem.find('a').attr('class') || '').replace(/(^|\s)active(\s|$)/, ' ').trim();
+ 
             filterList.map(function(el) {
-                filterHtml += '<li><a class="btn btn-md btn-primary-outline" href>' + el + '</a></li>';
+                filterHtml += '<li><a class="' + classAttr + '" href>' + el + '</a></li>';
             });
-            $section.find('.mbr-gallery-filter ul').append(allItem).append(filterHtml);
 
-        } else {
-            $section.find('.mbr-gallery-item__hided').removeClass('mbr-gallery-item__hided');
-            $section.find('.mbr-gallery-row').trigger('filter');
+            $section.find('.mbr-gallery-filter ul').append(filterHtml);
+
         }
 
         updateMasonry(event);
